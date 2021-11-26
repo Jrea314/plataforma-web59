3503904d6659f555311769f14f8c061bb889bd0b
Josue <jreay@est.ups.edu.ec>
2021-11-25 19:19:20 -0500

    Delete script.js

--- code/html/javascript-doom/taller1/script.js
+++
@@ -1,8 +0,0 @@
-function cargarElemento() {
-    var h1 = document.createElement('h1')
-    var contenido = document.createTextNode('DOM - Document Object Model')
-    h1.appendChild(contenido)
-
-    var container = document.getElementById('container')
-    container.appendChild(h1)
-}
