82781a81cda7430adc94612989b0e4d4de89db89
Josue <jreay@est.ups.edu.ec>
2021-12-02 16:42:11 -0500

    Create script.js

---
+++ code/html/javascript-basic/taller1/script.js
@@ -0,0 +1,47 @@
+// Tipado dinamico
+var  a  =  42
+consola . registro (  a  )
+
+a  =  'Hola mundo'
+consola . registro (  a  )
+
+a  =  verdadero
+consola . registro (  a  )
+
+// Tipos de datos primitivos
+var  a  =  2
+var  b  =  3.2
+consola . log (  typeof  una  )
+consola . log (  typeof  b  )
+
+var  c  =  verdadero
+consola . log (  typeof  c  )
+
+var  d  =  'Hola mundo'
+consola . log (  typeof  d  )
+
+var  e
+consola . log (  typeof  e  )
+consola . registro (  e  )
+
+var  d  =  nulo
+consola . log (  typeof  d  )
+consola . registro (  d  )
+
+var  x  =  2n  **  53n
+consola . log (  typeof  x  )
+
+consola . registro (  x  )
+
+// objetos
+var  persona  =  {
+    nombre : 'Guillermo' ,
+    apellido : 'Pizarro' ,
+    ciudad : 'Guayaquil'
+}
+
+consola . log (  persona  )
+consola . log (  typeof  persona  )
+
+persona . nombre  =  'Guillermo Omar'
+consola . log (  persona  )
